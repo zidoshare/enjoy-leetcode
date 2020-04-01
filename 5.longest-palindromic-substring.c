294babe5370654a4001d59ae1b19000f15370b7b
zido <wuhongxu1208@gmail.com>
2020-04-02 01:21:14 +0800

    finish 5,6,7

---
+++ 5.longest-palindromic-substring.c
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=5 lang=c
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (28.89%)
+ * Total Accepted:    839.8K
+ * Total Submissions: 2.9M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, find the longest palindromic substring in s. You may
+ * assume that the maximum length of s is 1000.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ */
+#include <string.h>
+#include <malloc.h>
+
+char * longestPalindrome(char * s){
+  int i = 0;
+  if(*s == '\0'){
+    return "";
+  }
+  int max = 1,start = 0;
+  for(;*(s + i) != '\0';i++) {
+    int j = 1,len = 1;
+    for(; i - j >= 0 && *(s + i + j) != '\0'; j++) {
+      if(*(s + i - j) == *(s + i + j)){
+        len += 2;
+      }else{
+        break;
+      }
+    }
+    if(len > max){
+      max = len;
+      start = i - j + 1;
+    }
+    for(len = 0,j = 0;i - j >= 0 && *(s + i + j + 1) != '\0'; j++) {
+      if(*(s + i - j) == *(s + i + j + 1)){
+        len += 2;
+      }else{
+        break;
+      }
+    }
+    if(len > max) {
+      max = len;
+      start = i - j + 1;
+    }
+  }
+  char* result = (char*)malloc(sizeof(char) * (max + 1));
+  *(result + max) = '\0';
+  strncpy(result,s + start,max);
+  return result;
+}
---
+++ 6.zigzag-conversion.c
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=6 lang=c
+ *
+ * [6] ZigZag Conversion
+ *
+ * https://leetcode.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (35.04%)
+ * Total Accepted:    424.1K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+ * of rows like this: (you may want to display this pattern in a fixed font for
+ * better legibility)
+ * 
+ * 
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 
+ * 
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * 
+ * Write the code that will take a string and make this conversion given a
+ * number of rows:
+ * 
+ * 
+ * string convert(string s, int numRows);
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "PAYPALISHIRING", numRows = 4
+ * Output: "PINALSIGYAHRPI"
+ * Explanation:
+ * 
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ * 
+ */
+
+#include <malloc.h>
+#include <string.h>
+char * convert(char * s, int numRows){
+  if(numRows == 1){
+    return s;
+  }
+  int len = strlen(s);
+  char* result = (char*)malloc(sizeof(char) * (len + 1));
+  result[len] = '\0';
+  int i = 0,index = 0;
+  for(;i < numRows;i++){
+    int j = 0;
+    while(1){
+      if(i < numRows - 1){
+        int target = i + 2 * (numRows - 1) * j;
+        if(target < len){
+          result[index++] = s[target];
+        }else{
+          break;
+        }
+      }
+      if(i > 0) {
+        int target = (j + 1) * (2 * (numRows - 1)) - i;
+        if(target < len){
+          result[index++] = s[target];
+        }else{
+          break;
+        }
+      }
+      j++;
+    }
+  }
+  return result;
+}
